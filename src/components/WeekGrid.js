f892fb1c26087d1b9bfa47d42fb408127ae36cd9
Spencer Wood <spencercwood@gmail.com>
2018-03-09 21:12:18 -0700

    Make week view correlate to the correct dates (#15)

--- src/components/WeekGrid.js
+++ src/components/WeekGrid.js
@@ -2,15 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import format from 'date-fns/format'
 import addDays from 'date-fns/add_days'
+import startOfWeek from 'date-fns/start_of_week'
 
 import GridSquare from './GridSquare'
 
 const WeekGrid = ({onGridClick, habit}) => {
   // get date at start of the week
-  const today = new Date()
+  const sunday = startOfWeek(new Date())
   const gridSquares = []
   for (let i = 0; i < 7; i++) {
-    const date = addDays(today, i + 1)
+    const date = addDays(sunday, i)
     const formattedDate = format(date, 'YYYY-MM-DD')
     gridSquares.push(
       <GridSquare
